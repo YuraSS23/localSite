984daaf193915cffe48149509dee48fe82540b03
YuraSS23 <nikitsenko_yura@mail.ru>
2024-03-01 11:13:04 +0300

    burger menu

--- src/components/general/Header.tsx
+++ src/components/general/Header.tsx
@@ -1,41 +1,40 @@
-import React, {useState} from 'react';
+import React from 'react';
 import s from "./Header.module.css"
 import peleng from "../../assets/images/peleng.svg"
-import menuIcon from "../../assets/images/free-icon-menu-4254068.png";
-import {NavLink} from "react-router-dom";
+import "./Header.css"
 
 export const Header = () => {
-    const [isVisible,setIsVisible] = useState(false)
     return (
-        <div className={s.header}>
-            <img className={s.logo} src={peleng}/>
-            <nav className={s.mobileMenuPicture} onClick={() => setIsVisible(true)}>
-                <img src={menuIcon} width={"50px"} height={"50px"}/>
-            </nav>
-            {isVisible && <div className={s.mobileMenu}>
-                <div className={s.item}>
-                    <button className={"closeBtn"} onClick={() => setIsVisible(false)}>X</button>
-                    <NavLink to={'/profile'}>
-                        ГЛАВНАЯ</NavLink>
-                    <div className={s.bugerMenu}>
-                        <div className={s.bugerMenuSecondary}>
-                            <a>Новости</a>
-                            <div className={s.bugerMenuSecondaryItems}>
-                                <a>Все новости</a>
-                                <a>День качества</a>
-                                <a>Видеосюжеты</a>
-                                <a>Мероприятия</a>
-                            </div>
-                        </div>
-                        <a>Опросы</a>
-                        <a>Приказы и распоряжения</a>
-                    </div>
-                </div>
-                <div className={s.item}><NavLink to={'/dialogs'}>О КОМПАНИИ</NavLink></div>
-                <div className={s.item}><NavLink to={'/users'}>КАТАЛОГИ</NavLink></div>
-                <div className={s.item}><NavLink to={'/news'}>СПРАВОЧНИКИ</NavLink></div>
-                <div className={s.item}><NavLink to={'/music'}>СОТРУДНИКУ</NavLink></div>
-            </div>}
+        <div className={"header"}>
+            <img className={"logo"} src={peleng}/>
+            <div className="hamburger-menu">
+                <input id="menu__toggle" type="checkbox"/>
+                <label className="menu__btn" htmlFor="menu__toggle">
+                    <span></span>
+                </label>
+                <ul className="menu__box">
+                    <li className={"mainPage"}>
+                        <a className="menu__item" href="#">ГЛАВНАЯ</a>
+                        <ul className="second__menu__items">
+                            <li className={"news"}>
+                                <a href="#" className="menu__item">Новости</a>
+                                <ul className="third__menu__items">
+                                    <li><a href="#" className="menu__item">Все новости</a></li>
+                                    <li><a href="#" className="menu__item">День качества</a></li>
+                                    <li><a href="#" className="menu__item">Видеосюжеты</a></li>
+                                    <li><a href="#" className="menu__item">Мероприятия</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#" className="menu__item">Опросы</a></li>
+                            <li><a href="#" className="menu__item">Приказы и распоряжения</a></li>
+                        </ul>
+                    </li>
+                    <li><a className="menu__item" href="#">О КОМПАНИИ</a></li>
+                    <li><a className="menu__item" href="#">КАТАЛОГИ</a></li>
+                    <li><a className="menu__item" href="#">СПРАВОЧНИКИ</a></li>
+                    <li><a className="menu__item" href="#">СОТРУДНИКУ</a></li>
+                </ul>
+            </div>
         </div>
     );
 };
